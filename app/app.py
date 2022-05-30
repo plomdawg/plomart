d7d1342d305cc882b250f37c1703428a3c8eaee0
Avalon Parton <avalonlee@gmail.com>
2022-05-30 02:00:02 -0700

    add redis

--- app/app.py
+++ app/app.py
@@ -2,12 +2,17 @@ import flask
 import plomart
 import io
 import os
+import redis
+
 
 app = flask.Flask(__name__)
 
 # Get app IP and port from env vars, default to http://localhost:5000
 APP_IP = os.environ.get("APP_IP", "127.0.0.1")
 PORT = os.environ.get("PORT", 5000)
+
+# Connect to the Redis server running in heroku
+rd = redis.from_url(os.environ.get("REDIS_URL"))
 
 
 @app.route('/favicon.ico')
@@ -34,6 +39,9 @@ def character():
 
     # Move to beginning of file so `send_file()` it will read from start
     file_object.seek(0)
+
+    # Increment counter of faces generated
+    faces_generated = rd.incr("faces_generated")
 
     return flask.send_file(file_object, mimetype='image/PNG')
 
